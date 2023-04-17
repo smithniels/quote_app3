7bf64745f454bb94689c8293ada45f69dafc9351
Niels Smith <30351969+smithniels@users.noreply.github.com>
2023-04-17 13:32:43 +0200

    hit enter

--- src/components/Randomizer.js
+++ src/components/Randomizer.js
@@ -11,7 +11,7 @@ function shuffle(array) {
   while (counter > 0) {
     // Pick a random index
     let index = Math.floor(Math.random() * counter);
-    console.log("this is from the Randomizer.js");
+    // console.log("this is from the Randomizer.js");
     console.log(index); // just curious - ns
     // Decrease counter by 1
     counter--;
@@ -20,6 +20,7 @@ function shuffle(array) {
     array[counter] = array[index];
     array[index] = temp;
   }
+
   return array;
 }
 
